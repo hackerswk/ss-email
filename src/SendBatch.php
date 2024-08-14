3cf4290cf4b8bf566546dd097d6508eb12d59518
wu gong Sie <swookon@gmail.com>
2024-08-14 11:34:38 +0800

    Add delay seconds

--- src/SendBatch.php
+++ src/SendBatch.php
@@ -129,7 +129,7 @@ class SendBatch
                     echo 'update mail pool failure!';
                     return false;
                 }
-                sleep(2); // 延遲 2 秒
+                sleep(3); // 延遲 2 秒
             }
         } catch (Exception $e) {
             //throw new Exception($e->getMessage());
